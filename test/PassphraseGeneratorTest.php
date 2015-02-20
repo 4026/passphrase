34fb3cb9bd352430058d42896a67214104ebbf08
4026 <name4026+github@gmail.com>
2015-02-20 18:09:39 +0000

    Fixing incorrect directory separator in test

--- test/PassphraseGeneratorTest.php
+++ test/PassphraseGeneratorTest.php
@@ -6,7 +6,7 @@ class PassphraseGeneratorTest extends PHPUnit_Framework_TestCase
 {
     function testGenerate()
     {
-        $generator = new Four026\Passphrase\PassphraseGenerator(__DIR__ . '\testWordList');
+        $generator = new Four026\Passphrase\PassphraseGenerator(__DIR__ . '/testWordList');
         $phrase = $generator->generate(5, ',');
         $this->assertCount(5, explode(',', $phrase), "Generated passphrase - '$phrase' - doesn't contain the expected number of words and/or uses the wrong separator.");
         $this->assertGreaterThan(10, strlen($phrase), "Generated passphrase - '$phrase' - isn't as long as expected.");
